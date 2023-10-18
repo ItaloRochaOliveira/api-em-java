bc61834a61467b856a62f1850a626048b0f7ef51
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-10-18 14:25:59 -0300

    INIT: First commit

---
+++ src/main/java/edu/api/ApiEmJavaApplication.java
@@ -0,0 +1,13 @@
+package edu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmJavaApplication.class, args);
+	}
+
+}
